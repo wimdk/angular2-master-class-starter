f7dccd2050cd1025845811c352befa6cfa2fbf2d
De Keyzer Wim <wim.dekeyzer@gmail.com>
2017-03-06 11:09:45 +0100

    exercise 2

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class ContactsAppComponent {
   title = 'Angular 2 Master Class setup works!';
+  contact: Contact = {
+	  id: 6,
+	  name: 'Diana Ellis',
+	  email: '',
+	  phone: '',
+	  birthday: '',
+	  website: '',
+	  image: '/assets/images/6.jpg',
+	  address: {
+	    street: '6554 park lane',
+	    zip: '43378',
+	    city: 'Rush',
+	    country: 'United States'
+	  		}
+	};
 }
